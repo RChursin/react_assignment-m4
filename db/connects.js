99e27c77ca0b780244efe9c0afa769e75f3f25f6
ganstacoder <ganstacoder@gmail.com>
2024-05-19 02:15:41 -0700

    hide key

--- db/connects.js
+++ db/connects.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectToDatabase = async () => {
   try {
-    await mongoose.connect('mongodb+srv://ganstacoder:RChursinDB@sandbox.muayiud.mongodb.net/sample_supplies');
+    await mongoose.connect(process.env.MONGO_KEY);
     console.log('Connected to MongoDB');
   } catch (error) {
     console.error('Error connecting to MongoDB:', error);
