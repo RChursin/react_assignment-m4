3b9a7e21c41c35e08f85bc850fc394a4858c10a8
Roman Chursin <84048716+RChursin@users.noreply.github.com>
2024-05-18 21:08:28 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(5051, () => {
+  console.log('Server is running on port 5051 open http://localhost:5051/ in your browser');
+});
